5f1c5419fc26d931b9feeb27305019f40505baf0
AJ Clemente <areljann.c@gmail.com>
2025-03-12 12:28:46 -0400

    UC Berkeley CS 61B - Week 2 - Homework 0 - DrawTriangle

---
+++ 002_UC_Berkeley_CS_61B/002_Week_2/Homework_0/DrawTriangle.java
@@ -0,0 +1,22 @@
+public class DrawTriangle {
+    public static void drawTriangle(int n) {
+        int maxChar = n + (n - 1);
+        char[] line = new char[maxChar];
+        for (int i = 0; i < maxChar; i++) {
+            line[i] = ' ';
+        }
+
+        int lp, rp;
+        lp = rp = maxChar / 2;
+        for (int i = 0; i < n; i++) {
+            line[lp - i] = '*';
+            line[rp + i] = '*';
+            System.out.println(new String(line));
+        }
+    }
+
+    public static void main(String[] args) {
+        drawTriangle(5);
+        drawTriangle(10);
+    }
+}
